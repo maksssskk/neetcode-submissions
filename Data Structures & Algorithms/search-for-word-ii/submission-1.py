66b9950005c9a71903c3468c292342e00cf41920
Maksym Kot <yfw86dk2hj@privaterelay.appleid.com>
2026-09-05 22:55:24 +0300

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,50 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = None
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, word):
+        node = self.root
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+        node.word = word
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = Trie()
+        for w in words:
+            trie.insert(w)
+
+        res = []
+        h = len(board)
+        w = len(board[0])
+        def dfs(r, c, node):
+            if r < 0 or r >= h or c < 0 or c >= w: 
+                return
+                
+            el = board[r][c]
+            if el not in node.children:
+                return 
+            next_node = node.children[el]
+            if next_node.word:
+                res.append(next_node.word)
+                next_node.word = None
+
+            temp = board[r][c]
+            board[r][c] = '#'
+
+            dfs(r-1, c, next_node)
+            dfs(r+1, c, next_node)
+            dfs(r, c-1, next_node)
+            dfs(r, c+1, next_node)
+
+            board[r][c] = temp
+        for r in range(h):
+            for c in range(w):
+                dfs(r, c, trie.root)
+        return res
